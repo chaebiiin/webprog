4e27e0a2d88180ca7927cf6b718d62d5b97b96ec
cheabiiin <ravis_toy@naver.com>
2020-07-02 11:05:13 +0900

    20-07-02

---
+++ javascript/func.js
@@ -0,0 +1,40 @@
+var num;
+document.write(typeof num);
+document.write("<br>");
+
+num = 10;
+document.write(typeof num);
+document.write("<br>");
+
+num = "NICE";
+document.write(typeof num);
+document.write("<br>");
+
+num = {
+    name: "H",
+    age: 19
+}
+document.write(typeof num);
+
+var num1, num2, num3;
+num1 = "HELLO";
+num2 = 10;
+num3 = 5;
+num = num1 + (num2 + num3);
+console.log(num);
+
+num3 = "15";
+console.log(num2 == num3);
+var sum = 0;
+for (var i = 0; i < 10; i++) {
+        if(i<5)
+        continue;
+
+    sum += i;
+}
+console.log("결과 값은 : " +sum);
+
+var $sum = 10;
+var _sum = 20;
+var sum1 = null;
+var num1 = 10;
---
+++ javascript/func1.js
@@ -0,0 +1,44 @@
+function sum(a, b) {
+    console.log("sum(a,b) 호출");
+    var sum = a + b;
+    var result = document.getElementById("show");
+    result.innerHTML = sum;
+}
+
+function sum() {
+    console.log("sum 호출");
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+
+}
+function mySum() {
+    var result1 = sum(2, 4);
+    console.log("result1의 결과는 " + result1);
+}
+
+function arraySum(ary) {
+    var sum = 0;
+    for (ar of ary) {
+        sum += ar;
+    }
+    return sum;
+}
+function arrayTest() {
+    var numAry = [3, 4, 5, 6, 7];
+    var result2 = arraySum(numAry);
+    console.log(result2);
+}
+function funcTest(hellofunc) {
+    var hello = document.getElementById("show").innerHTML;
+    console.log(hellofunc);
+    hello += " ,World";
+    document.getElementById("show").innerHTML = hello;
+    
+}
+function hellofunc(){
+    console.log(hellofunc);
+    document.getElementById("show").innerHTML =  "HELLO!!";
+}
