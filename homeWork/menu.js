2758b82323e10b8a25be7a47e9a7f5ee4fcd3a9c
chaebin <ravis_toy@naver.com>
2020-07-13 23:26:34 +0900

    20-07-13 Home

--- homeWork/menu.js
+++ homeWork/menu.js
@@ -1,29 +1,46 @@
 var coffeeData = [{ "no": 1, "menuName": "Americano", "type": "hot/ice", "price": 3400 },
-{ "no": 2, "menuName": "latte", "type": "hot/ice", "price": 4000 },
+{ "no": 2, "menuName": "Latte", "type": "hot/ice", "price": 4000 },
 { "no": 3, "menuName": "Mocha", "type": "hot/ice", "price": 4500 },
-{ "no": 4, "menuName": "Cappuccino", "type": "hot/ice", "price": 5500 }]
+{ "no": 4, "menuName": "Cappuccino", "type": "hot/ice", "price": 5500 },
+{ "no": 5, "menuName": "Espresso", "type": "hot", "price": 5000 },
+{ "no": 6, "menuName": "Macchiato", "type": "hot/ice", "price": 4800 },
+{ "no": 7, "menuName": "Hazelnut", "type": "hot/ice", "price": 4600 }];
 var coffees = ['no', 'menuName', 'type', 'price'];
 
-var teaData = [{ "no": 1, "menuName": "Peachtea", "type": "hot/ice", "price": 3000},
-{ "no": 2, "menuName": "Greentea", "type": "hot/ice", "price": 4100 },
-{ "no": 3, "menuName": "Milktea", "type": "hot/ice", "price": 4600 },
-{ "no": 4, "menuName": "Honeytea", "type": "hot/ice", "price": 4500}]
+var teaData = [{ "no": 1, "menuName": "PeachTea", "type": "hot/ice", "price": 3000 },
+{ "no": 2, "menuName": "GreenTea", "type": "hot/ice", "price": 4100 },
+{ "no": 3, "menuName": "MilkTea", "type": "hot/ice", "price": 4600 },
+{ "no": 4, "menuName": "HoneyTea", "type": "hot/ice", "price": 4500 },
+{ "no": 5, "menuName": "GrainLatte", "type": "hot/ice", "price": 5500 },
+{ "no": 6, "menuName": "GingerTea", "type": "hot/ice", "price": 4300 },
+{ "no": 7, "menuName": "PeppermintTea", "type": "hot/ice", "price": 4500 }];
+
+
 var teas = ['no', 'menuName', 'type', 'price'];
 
 var dessertData = [{ "no": 1, "menuName": "Cake", "price": 3000 },
-{ "no": 2, "menuName": "Cookie", "price": 4100},
+{ "no": 2, "menuName": "Cookie", "price": 4100 },
 { "no": 3, "menuName": "Macaroon", "price": 4600 },
-{ "no": 4, "menuName": "HoneyBread", "price": 4500 }];
+{ "no": 4, "menuName": "HoneyBread", "price": 4500 },
+{ "no": 5, "menuName": "Chocoba", "price": 1500 },
+{ "no": 6, "menuName": "Waffle", "price": 4500 }];
 var desserts = ['no', 'menuName', 'price'];
 
-window.onload = function () {
+// window.onload = function () {
+//     createCoffeeTable(coffeeData);
+//     createTeaTable(teaData);
+//     createDessertTable(dessertData);
+// }
+
+$(document).ready(function () {
     createCoffeeTable(coffeeData);
-    createTeaTable(teaData);
-    createDessertTable(dessertData);
-}
+        createTeaTable(teaData);
+        createDessertTable(dessertData);
+});
+
 
 function createCoffeeTable() {
-    var $table = $('<table />').attr({ 'border': '1', 'cellPadding': '0px', 'cellSpacing': '0px'});
+    var $table = $('<table />').attr({ 'border': '1', 'cellPadding': '5px', 'cellSpacing': '5px' });
     var $tr = $('<tr />');
     coffees.forEach(function (o, i, a) {
         $tr.append($('<th />').text(o));
@@ -45,7 +62,7 @@ function createCoffeeTable() {
     $('#show1').append($table);
 }
 function createTeaTable() {
-    var $table = $('<table />').attr({ 'border': '1', 'cellPadding': '0px', 'cellSpacing': '0px' });
+    var $table = $('<table />').attr({ 'border': '1', 'cellPadding': '5px', 'cellSpacing': '5px' });
     var $tr = $('<tr />');
     teas.forEach(function (o, i, a) {
         $tr.append($('<th />').text(o));
@@ -66,7 +83,7 @@ function createTeaTable() {
     $('#show2').append($table);
 }
 function createDessertTable() {
-    var $table = $('<table />').attr({ 'border': '1', 'cellPadding': '0px', 'cellSpacing': '0px' });
+    var $table = $('<table />').attr({ 'border': '1', 'cellPadding': '5px', 'cellSpacing': '5px' });
     var $tr = $('<tr />');
     desserts.forEach(function (o, i, a) {
         $tr.append($('<th />').text(o));
@@ -80,12 +97,12 @@ function createDessertTable() {
         desserts.forEach(function (f) { //필드
             $tr.append($('<td />').text(o[f]));
         });
-        $tr.append($('<td />').append($('<input />').attr('type', 'checkbox')));
+        $tr.append($('<td />').append($('<input />').attr({ 'type': 'checkbox' })));
         $table.append($tr);
     });
 
-
     $('#show3').append($table);
+
 }
 
 function mouseenterfunc() {
@@ -93,4 +110,11 @@ function mouseenterfunc() {
 }
 function mouseoutFunc() {
     $(this).css('color', '');
+    console.log(this);
 }
+
+// function deleteFunc() {
+//     $($tr).remove();
+//     console.log($tr);
+// }
+
