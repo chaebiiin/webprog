4885beaa5d1d768bfb07f8af2e821c5e87542715
chaebin <ravis_toy@naver.com>
2020-07-15 00:00:23 +0900

    20-07-14 Home

--- homeWork/menu.js
+++ homeWork/menu.js
@@ -5,7 +5,7 @@ var coffeeData = [{ "no": 1, "menuName": "Americano", "type": "hot/ice", "price"
 { "no": 5, "menuName": "Espresso", "type": "hot", "price": 5000 },
 { "no": 6, "menuName": "Macchiato", "type": "hot/ice", "price": 4800 },
 { "no": 7, "menuName": "Hazelnut", "type": "hot/ice", "price": 4600 }];
-var coffees = ['no', 'menuName', 'type', 'price'];
+var coffees = ['no', 'menuName', 'type', 'price',''];
 
 var teaData = [{ "no": 1, "menuName": "PeachTea", "type": "hot/ice", "price": 3000 },
 { "no": 2, "menuName": "GreenTea", "type": "hot/ice", "price": 4100 },
@@ -24,7 +24,7 @@ var dessertData = [{ "no": 1, "menuName": "Cake", "price": 3000 },
 { "no": 4, "menuName": "HoneyBread", "price": 4500 },
 { "no": 5, "menuName": "Chocoba", "price": 1500 },
 { "no": 6, "menuName": "Waffle", "price": 4500 }];
-var desserts = ['no', 'menuName', 'price'];
+var desserts = ['no', 'menuName', 'price',' '];
 
 // window.onload = function () {
 //     createCoffeeTable(coffeeData);
@@ -40,7 +40,7 @@ $(document).ready(function () {
 
 
 function createCoffeeTable() {
-    var $table = $('<table />').attr({ 'border': '1', 'cellPadding': '5px', 'cellSpacing': '5px' });
+    var $table = $('<table />').attr({ 'border': '0', 'cellPadding': '5px', 'cellSpacing': '0px' });
     var $tr = $('<tr />');
     coffees.forEach(function (o, i, a) {
         $tr.append($('<th />').text(o));
@@ -48,21 +48,22 @@ function createCoffeeTable() {
     $table.append($tr);
 
     //데이터 영역
-    coffeeData.forEach(function (o) {
+    coffeeData.forEach(function (o,i) {
         $tr = $('<tr />').mouseenter(mouseenterfunc)
-            .mouseout(mouseoutFunc);
+            .mouseout(mouseoutFunc).attr('id','coffee_'+ i);
         coffees.forEach(function (f) { //필드
-            $tr.append($('<td />').text(o[f]))
+            if(o != '') $tr.append($('<td />').text(o[f]))
 
         });
-        $tr.append($('<td />').append($('<input />').attr('type', 'checkbox')));
+        
+        $tr.append($('<td />').append($('<input />').attr({'type': 'radio','name':'coffee', 'value':i})));
         $table.append($tr);
     });
 
     $('#show1').append($table);
 }
 function createTeaTable() {
-    var $table = $('<table />').attr({ 'border': '1', 'cellPadding': '5px', 'cellSpacing': '5px' });
+    var $table = $('<table />').attr({ 'border': '1', 'cellPadding': '5px', 'cellSpacing': '0px' });
     var $tr = $('<tr />');
     teas.forEach(function (o, i, a) {
         $tr.append($('<th />').text(o));
@@ -70,20 +71,20 @@ function createTeaTable() {
     $table.append($tr);
 
     //데이터 영역
-    teaData.forEach(function (o) {
+    teaData.forEach(function (o, i) {
         $tr = $('<tr />').mouseenter(mouseenterfunc)
-            .mouseout(mouseoutFunc);
+            .mouseout(mouseoutFunc).attr('id', 'tea_'+i);
         teas.forEach(function (f) { //필드
             $tr.append($('<td />').text(o[f]));
         });
-        $tr.append($('<td />').append($('<input />').attr('type', 'checkbox')));
+        $tr.append($('<td />').append($('<input />').attr({'type': 'radio','name':'tea', 'value':i})));
         $table.append($tr);
     });
 
     $('#show2').append($table);
 }
 function createDessertTable() {
-    var $table = $('<table />').attr({ 'border': '1', 'cellPadding': '5px', 'cellSpacing': '5px' });
+    var $table = $('<table />').attr({ 'border': '1', 'cellPadding': '5px', 'cellSpacing': '0px' });
     var $tr = $('<tr />');
     desserts.forEach(function (o, i, a) {
         $tr.append($('<th />').text(o));
@@ -91,32 +92,40 @@ function createDessertTable() {
     $table.append($tr);
 
     //데이터 영역
-    dessertData.forEach(function (o) {
+    dessertData.forEach(function (o,i) {
         $tr = $('<tr />').mouseenter(mouseenterfunc)
-            .mouseout(mouseoutFunc);
+            .mouseout(mouseoutFunc).attr('id', 'dessert_'+i);
         desserts.forEach(function (f) { //필드
             $tr.append($('<td />').text(o[f]));
         });
-        $tr.append($('<td />').append($('<input />').attr({ 'type': 'checkbox' })));
+        $tr.append($('<td />').append($('<input />').attr({ 'type': 'radio', 'name':'dessert', 'value':i })));
         $table.append($tr);
     });
 
     $('#show3').append($table);
 
 }
-
+// 달러(누구를?-CSS선택자방식).어떤행위(자세하게)
 function mouseenterfunc() {
-    $(this).css('color', 'red');
+    $(this).css('color', 'red');    
+    console.log($(this));
+    console.log('===========구분자============');
+    console.log(this);
 }
 function mouseoutFunc() {
     $(this).css('color', '');
     console.log(this);
 }
 
-// function deleteFunc() {
-//     $($tr).remove();
-//     console.log($tr);
-// }
+function deleteFunc(name) {
+    var value= $('input[name='+name+']:checked').val();
+    console.log('input[name='+name+']:checked');
+    console.log('value = '+value);
+    var id = name+'_'+value;
+    console.log('id = '+id);
+    $('#'+id).remove();
+    console.log(id +"가 삭제되었습니다")
+}
 
 
 
